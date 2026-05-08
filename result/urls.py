d0a34e341c3ce35028113bab6e766d70b9c80c2c
ozztozz <tuncozden@gmail.com>
2026-05-08 13:41:52 +0300

    new page

--- result/urls.py
+++ result/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import startlist_page
+from .views import startlist_page, htmx_club_list
 
 app_name = "result"
 
 urlpatterns = [
     path("", startlist_page, name="startlist_page"),
+    path("htmx/club-list/", htmx_club_list, name="htmx_club_list"),
 ]
--- result/views.py
+++ result/views.py
@@ -1,7 +1,36 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from .models import StartListEntry
 
 @login_required
-def startlist_page(request):
+def startlist_page_old(request):
 	return render(request, "result/startlist.html")
+
+
+def startlist_page(request):
+	events = (
+		StartListEntry.objects.exclude(event_title__isnull=True)
+		.exclude(event_title="")
+		.values("event_title", "event_location", "event_date")
+		.distinct()
+		.order_by("event_date","event_title", "event_location",)
+	)
+	clubs=(
+		StartListEntry.objects.exclude(club_raw__isnull=True)
+		.exclude(club_raw="")
+		.values("club_raw")
+		.order_by("club_raw")
+		.distinct()
+	)
+	
+	print("Clubs:", len(clubs))
+	return render(request, "result/startlist.html", {"events": events, "clubs": clubs})
+
+
+def htmx_club_list(request):
+	club_raw = request.GET.get("club_search")
+	if club_raw:
+		clubs = StartListEntry.objects.filter(club_raw__icontains=club_raw).values("club_raw").order_by("club_raw").distinct()
+	else:
+		clubs = StartListEntry.objects.values("club_raw").distinct()
+	return render(request, "result/partials/htmx_club_list.html", {"clubs": clubs})
